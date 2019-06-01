84ee1a325b13793ca8771e52bb6328cf2b79dc79
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-06-01 20:26:56 +0200

    [Responders] Adding view responder

---
+++ MorvanRespondersBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of MorvanRespondersBundle
+ *
+ * (c) Aurelien Morvan <morvan.aurelien@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Morvan\Bundle\RespondersBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MorvanRespondersBundle
+ */
+class MorvanRespondersBundle extends Bundle
+{
+}
